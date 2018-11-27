fd6ec64f9c8a8fb6f7b566a2ca4324104070c32a
Guillaume <ga@shpock.com>
2018-11-27 17:35:11 +0100

    made apiv2 compatible

--- pagerduty-desktop-notifications.py
+++ pagerduty-desktop-notifications.py
@@ -18,15 +18,16 @@ log = logging.getLogger('pagerduty')
 token = ''
 user_name = ''
 
-api = 'https://finderly.pagerduty.com/api/v1/'
-headers = {'Content-type': 'application/json',
+api = 'https://api.pagerduty.com/'
+headers = {'Accept': 'application/vnd.pagerduty+json;version=2',
+           'Content-type': 'application/json',
            'Authorization': 'Token token=%s' % token}
 
 pynotify.init('Pagerduty issue applet')
 
 # get user id from user name
 def getUserId(name):
-    endpoint = 'https://finderly.pagerduty.com/api/v1/' + 'users'
+    endpoint = api + 'users'
     query = {'query': name}
     r = requests.get(endpoint, params=query, headers=headers)
     r.raise_for_status()
@@ -37,10 +38,10 @@ def getUserId(name):
 
 # poll pd api for incidents
 def poll(userid):
-    endpoint = 'https://finderly.pagerduty.com/api/v1/' + 'incidents'
-    params = {'assigned_to': userid,
+    endpoint = 'https://api.pagerduty.com/' + 'incidents'
+    params = {'include[]': 'assignees',
           'sort_by': 'urgency:asc,incident_number:desc',
-          'status': 'triggered'}
+          'statuses[]': 'triggered'}
     r = requests.get(endpoint, params=params, headers=headers)
     r.raise_for_status()
     for incident in r.json().get('incidents', []):
@@ -51,14 +52,15 @@ def poll(userid):
 # show notification on screen
 def show(userid, incident):
     body_int = json.loads(json.dumps(incident))
-    user = body_int['assigned_to_user']['id']
+    user = body_int['assignments'][0]['assignee']['id']
 
     if user == userid:
         summary = 'PagerDuty: ' + body_int['urgency']
-        if 'description' in body_int['trigger_summary_data']:
-            body = body_int['trigger_summary_data']['description']
-        else:
-            body = body_int['trigger_summary_data']['subject']
+       # if 'description' in body_int['trigger_summary_data']:
+       #     body = body_int['trigger_summary_data']['description']
+       # else:
+       #     body = body_int['trigger_summary_data']['subject']
+        body = body_int['summary']
         msg = pynotify.Notification(summary, body)
         msg.set_urgency(pynotify.URGENCY_NORMAL)
         if not msg.show():
